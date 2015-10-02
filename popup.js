89f04ba6a95c06d018535a3c0697927b8c2eea69
Patrick Everman <peverman@squarespace.com>
2015-10-02 17:02:31 -0400

    first commit

---
+++ popup.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var loadButton = document.getElementById('loadData');
+  loadButton.addEventListener('click', function() {
+    
+    console.log("button has been clicked!");
+    //until hasMore == false do 
+    document.querySelector('.load-more').click();
+        
+
+    });
+  }, false);
+}, false);
