cc186048cc01b09a972d5f14ad4759b269797176
Patrick Everman <everman.patrick@gmail.com>
2015-11-25 17:19:59 -0500

    added animation and search function

--- content.js
+++ content.js
@@ -3,17 +3,40 @@ var intervalFunc = setInterval(function() {
     var btn = document.querySelector('.load-more');
     if(btn) {
       btn.click();
+      console.log("new page rendered");
     }
     else {
-    	alert("Events have finished loading");
     	clearInterval(intervalFunc);
+      chrome.tabs.getSelected(null, function(tab) {
+        chrome.tabs.sendMessage(tab.id, { text: "EVENTS_LOADED" });
+      });
     }
-  }, 500);
+  }, 500);  
 }
+
+function hideViewsAndClicks(e) {
+  console.log(e);
+  var events = document.querySelectorAll('.events li');
+    for(var i = 0; i < events.length; i++) {
+      var given_event = events[i].querySelector('.name');
+      var regex = new RegExp( e, 'g' );
+        if ( given_event.innerHTML.match(regex) ) {
+          
+        }
+        else {
+          given_event.parentNode.parentNode.style.display="none";
+        }
+  }
+}
+
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 
   if (msg.text == "BUTTON_CLICKED") {
-	   loadEvents();
+    loadEvents();
    }
+   else if(msg.text == "FILTER_BUTTON_CLICKED") {
+    hideViewsAndClicks(msg.filter);
+           
+  }
 });
--- popup.js
+++ popup.js
@@ -6,6 +6,27 @@ document.addEventListener('DOMContentLoaded', function() {
     chrome.tabs.getSelected(null, function(tab) {
         chrome.tabs.sendMessage(tab.id, { text: "BUTTON_CLICKED" });
     });
+    
+    document.body.classList.add("animation", "animation-gif"); 
+  });
+
+
+
+  chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+
+  if (msg.text == "EVENTS_LOADED") {
+    console.log("event load msg received");
+    document.body.classList.remove("animation", "animation-gif");
+   }
+});
+
+
+  var filterButton = document.getElementById('filterData');
+  filterButton.addEventListener('click', function() {
+  	console.log("filter button clicked");
+    chrome.tabs.getSelected(null, function(tab) {
+        chrome.tabs.sendMessage(tab.id, { text: "FILTER_BUTTON_CLICKED", filter: document.querySelector('.text').value });
+    });
 
   });
 
